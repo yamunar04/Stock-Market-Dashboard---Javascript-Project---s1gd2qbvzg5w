42df3c255d3f71099c4a377bc841659db1e30270
yamunar04 <yamunar0411@gmail.com>
2023-11-02 10:37:38 +0530

    updated files

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-const apiKey = 'AXG6NJXXDIDHEXDZ';
-const apiUrl = "https://www.alphavantage.co/query";
+const API_KEY = 'AXG6NJXXDIDHEXDZ';
+const API_URL = "https://www.alphavantage.co/query";
+const cardContainer = document.getElementById("card-container");
 
 document.getElementById("addStockBtn").addEventListener("click", addStockToWatchlist);
 
-function addStockToWatchlist() {
+async function addStockToWatchlist() {
   const symbol = document.getElementById("symbolInput").value;
   const timeFrame = document.getElementById("timeOptions").value;
-  if(symbol)
-  {
-    fetchStockData(symbol, timeFrame);
+  if (symbol) {
+    await fetchStockData(symbol, timeFrame);
+    renderCard(dataArr[1], symbol, timeFrame);
   }
-  else{
+  else {
     alert('Enter Symbol');
   }
 }
 
-
+let dataArr = [];
 async function fetchStockData(symbol, timeFrame) {
   try {
-    const url = `${apiUrl}?function=TIME_SERIES_${timeFrame}&symbol=${symbol}&interval=5min&apikey=${apiKey}`;
+    const url = `${API_URL}?function=TIME_SERIES_${timeFrame}&symbol=${symbol}&interval=15min&apikey=${API_KEY}`;
     const response = await fetch(url);
     const data = await response.json();
     console.log("fetchedData: ", data);
-    let dataArr = Object.entries(data);
-    console.log("dataArr: ", dataArr);
-    console.log("dataArr[1]: ", dataArr[1]);
-    renderCard(dataArr[1], symbol, timeFrame);
+    dataArr = Object.entries(data);
+
   } catch (error) {
     console.error(error);
     throw error;
   }
 }
 
-const cardContainer = document.getElementById("card-container");
 
 function renderCard(stock, symbol, timeFrame) {
+  console.log(stock);
   console.log(symbol, timeFrame);
   const card = document.createElement("div");
   card.classList.add("card");
-  card.dataset.symbol = symbol; 
+  card.dataset.symbol = symbol;
   card.dataset.cardName = `${symbol.toLowerCase()}-${timeFrame.toLowerCase()}`;
   card.innerHTML = `
         <div class="card-heading">
@@ -50,29 +49,37 @@ function renderCard(stock, symbol, timeFrame) {
                 <div id="modal" class="modal"></div>
             </div> `;
 
-  cardContainer.appendChild(card);
-
   const stockSymbol = card.querySelector(".stock-symbol");
   stockSymbol.addEventListener("click", (event) => {
     event.stopPropagation();
     showStockData(stock, symbol, timeFrame);
   });
 
-
   const deleteButton = card.querySelector(".delete-button");
   deleteButton.addEventListener("click", (event) => {
-    event.stopPropagation(); 
-    deleteCard(symbol);
+    event.stopPropagation();
+    deleteCard(symbol, timeFrame);
   });
+
+  cardContainer.appendChild(card);
+
 }
-function deleteCard(symbol) {
-  const card = cardContainer.querySelector(`[data-symbol="${symbol}"]`);
+
+function deleteCard(symbol, timeFrame) {
+  const card = cardContainer.querySelector(`[data-card-name = "${symbol.toLowerCase()}-${timeFrame.toLowerCase()}"]`);
   if (card !== null) {
     card.remove();
   }
 }
 
 function showStockData(stock, symbol, timeFrame) {
+  console.log(stock);
+
+  const header = document.createElement("h2");
+  const table = document.createElement("table");
+  const headerRow = document.createElement("tr");
+  const timeFrameData = Object.entries(stock[1]).slice(0, 10);
+  const headers = ["Date", "Open", "High", "Low", "Close", "Volume"];
   let cards = cardContainer.querySelectorAll(".card");
   cards = Array.from(cards);
   console.log("cards: ", cards);
@@ -86,20 +93,15 @@ function showStockData(stock, symbol, timeFrame) {
 
   console.log("selectedCard: ", selectedCard[0]);
   console.dir(selectedCard[0].querySelector('.modal-container'));
-  
+
   let modalContainer = selectedCard[0].querySelector('.modal-container');
   let modal = selectedCard[0].querySelector('.modal');
 
   console.log(modalContainer);
   console.log(modal);
 
-  const header = document.createElement("h2");
   modalContainer.style.display = "block";
 
-  const table = document.createElement("table");
-  const headerRow = document.createElement("tr");
-
-  const headers = ["Date", "Open", "High", "Low", "Close", "Volume"];
 
   headers.forEach((headerText) => {
     const th = document.createElement("th");
@@ -109,14 +111,13 @@ function showStockData(stock, symbol, timeFrame) {
 
   table.appendChild(headerRow);
 
-  const timeFrameData = Object.entries(stock[1]).slice(0, 10);
   console.log(timeFrameData);
 
   timeFrameData.forEach(([date, dataSet]) => {
     const row = document.createElement("tr");
 
     const fields = ["1. open", "2. high", "3. low", "4. close", "5. volume"];
-    
+
     const dateCell = document.createElement("td");
     dateCell.innerText = date;
     row.appendChild(dateCell);
